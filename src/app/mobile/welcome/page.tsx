31887f22bbc76be12e33be223c8d886c03f68a85
Linn-Htet123 <linnhtet6102005@gmail.com>
2024-05-28 00:29:41 +0630

    added welcome skeleton

--- src/app/mobile/welcome/page.tsx
+++ src/app/mobile/welcome/page.tsx
@@ -1,6 +1,17 @@
-import WelcomeWrapper from "@/components/mobile/welcome/welcome";
+import WelcomeSkeleton from "@/components/mobile/welcome/welcomeSkeleton";
+import dynamic from "next/dynamic";
+
+// Lazy load the WelcomeWrapper component
+const WelcomeWrapper = dynamic(
+  () => import("@/components/mobile/welcome/welcome"),
+  {
+    ssr: false,
+    loading: () => <WelcomeSkeleton />, // Optional: Add a loading indicator
+  }
+);
 
 const Welcome = () => {
   return <WelcomeWrapper />;
 };
+
 export default Welcome;

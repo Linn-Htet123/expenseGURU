e050da48edd57676576d677115609fb453f2ca3b
Linn-Htet123 <linnhtet6102005@gmail.com>
2024-05-28 14:33:36 +0630

    feat/GURU: Added auth UI

---
+++ src/app/mobile/login/page.tsx
@@ -0,0 +1,58 @@
+import Bg from "../../../../public/welcome-bg.png";
+import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { getMobileRoute } from "@/utils/route";
+import { Route } from "@/enums/route";
+const Login = () => {
+  return (
+    <div
+      className="h-dvh flex flex-col justify-center items-center"
+      style={{
+        backgroundImage: `url(${Bg.src})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "144%",
+        backgroundPosition: "top center",
+      }}
+    >
+      <Card className="mx-auto w-[340px] max-w-sm drop-shadow-2xl shadow-primary">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold">Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                placeholder="m@example.com"
+                required
+                type="email"
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" required type="password" />
+            </div>
+            <Button className="w-full" type="submit">
+              Login
+            </Button>
+            <Label className="text-center w-full flex justify-center">
+              Have an account?
+              <Link
+                href={getMobileRoute(Route.LOGIN)}
+                className="text-primary ml-2"
+              >
+                Login
+              </Link>
+            </Label>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;

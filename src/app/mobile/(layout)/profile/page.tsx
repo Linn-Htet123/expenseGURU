1415a21432a7a21e49a566f4a8915e368b7483d6
Linn-Htet123 <linnhtet6102005@gmail.com>
2024-06-06 22:55:19 +0630

    feat/GURU : mobile profile page aligned

--- src/app/mobile/(layout)/profile/page.tsx
+++ src/app/mobile/(layout)/profile/page.tsx
@@ -1,12 +1,38 @@
-import dynamic from "next/dynamic";
-const WithBackground = dynamic(
-  () => import("@/components/common/withBackground"),
-  {
-    ssr: false,
-  }
-);
+"use client";
+import ProfileHead from "@/components/common/profileHead";
+import Image from "next/image";
+import Bg from "../../../../../public/home-bg.png";
+import Logout from "@/components/common/logout";
+import { ExitIcon } from "@radix-ui/react-icons";
+import { useLogout } from "@/hooks/useLogout";
+
 const Profile = () => {
-  return <WithBackground className="pt-10">helo</WithBackground>;
+  const { logout } = useLogout();
+  const handleLogout = async () => {
+    await logout();
+  };
+
+  return (
+    <div className="pt-10 flex flex-col items-center justify-center">
+      <div className="absolute top-0 left-0">
+        <Image src={Bg} alt="background image" className="w-screen" />
+        <div className="relative bottom-[80px]">
+          <ProfileHead />
+          <div className="px-4 mt-4 w-full">
+            <div
+              className="flex items-center justify-between w-full py-4"
+              onClick={handleLogout}
+            >
+              <div>Logout</div>
+              <div>
+                <ExitIcon width={30} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;

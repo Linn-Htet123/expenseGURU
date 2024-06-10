eeeb560c4ce38f46b5f9a83c45462c4bc3816c5b
Linn-Htet123 <linnhtet6102005@gmail.com>
2024-06-11 00:15:46 +0630

    feature/GURU : chart added for test

---
+++ src/components/ui/barChart.tsx
@@ -0,0 +1,60 @@
+// components/BarChart.js
+"use client";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const BarChart = () => {
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        label: "Income",
+        data: [1500, 1700, 2000, 1800, 2200, 2500],
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+      {
+        label: "Expense",
+        data: [1000, 1200, 1300, 1100, 1400, 1600],
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Income and Expense Tracker",
+      },
+    },
+  };
+
+  return <Bar data={data} options={options} height={200} />;
+};
+
+export default BarChart;

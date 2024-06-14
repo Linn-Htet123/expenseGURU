690f40966952f136d9f2df991ec44845cf4d0f87
Thar Linn Htet <87564766+Linn-Htet123@users.noreply.github.com>
2024-06-15 00:01:58 +0630

    Develop (#34)
    * feat: [GURU] transaction api (#29)
    
    * Feature/guru api transaction category filter (#31)
    
    * Develop (#25)
    
    * feat/GURU : layout added for mobile with footer and toast added
    
    * feat/GURU : layout added for mobile with footer and toast added
    
    * feat/GURU : login validation with error message and show hide password done
    
    * feat/ GURU: api bind to signup and login with validation and middlware done
    
    * feat/GURU : middleware protection fully done
    
    * feat/GURU : added logout and greeting
    
    * feat/GURU: added mobile header
    
    * Feature/guru api category (#22)
    
    * feat: [GURU] on progress category
    
    * feat: [GURU] category update
    
    * feat: [GURU] delete category feature
    
    * feat: [GURU] category pagination
    
    * feat/GURU : small changes
    
    * feat/GURU : mobile profile page aligned
    
    * feat/GURU: wallet ui in progress
    
    * feat/GURU : wallet design done
    
    ---------
    
    Co-authored-by: arkar93 <32826734+arkar93@users.noreply.github.com>
    
    * packge.lock.json
    
    * added current tab on wallet for mock
    
    * feat/GURU: added suspense
    
    * fix/GURU: added route to wallet link
    
    * feat: [GURU] transaction api (#29) (#30)
    
    Co-authored-by: arkar93 <32826734+arkar93@users.noreply.github.com>
    
    * feat: [GURU] transaction type and category search filter
    
    * feat: [GURU] date filter
    
    ---------
    
    Co-authored-by: Thar Linn Htet <87564766+Linn-Htet123@users.noreply.github.com>
    Co-authored-by: Linn-Htet123 <linnhtet6102005@gmail.com>
    
    * Feature/guru mobile category (#33)
    
    * feature/GURU : chart added for test
    
    * featutre/GURU: added the category page
    
    * feature/GURU: add category page list and form done
    
    * fix/GURU: edit the category and add the dialog
    
    * fix/GURU: fix the home page
    
    ---------
    
    Co-authored-by: arkar93 <32826734+arkar93@users.noreply.github.com>
    Co-authored-by: arkar93 <arkarkyaw.akk93@gmail.com>

--- src/app/mobile/(layout)/home/page.tsx
+++ src/app/mobile/(layout)/home/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import Bg from "../../../../../public/home-bg.png";
 import IncomeArrow from "../../../../../public/income-arrow.png";
 import ExpenseArrow from "../../../../../public/expense-arrow.png";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -9,14 +10,7 @@ import Logout from "@/components/common/logout";
 import greetPlugin from "@/utils/frontend/date";
 import dayjs from "dayjs";
 import Greeting from "@/components/common/greeting";
-import dynamic from "next/dynamic";
 
-const WithBackground = dynamic(
-  () => import("@/components/common/withBackground"),
-  {
-    ssr: false,
-  }
-);
 const HomePage = () => {
   dayjs.extend(greetPlugin);
   const { user, getLoggedInUserData } = useLogin();
@@ -24,84 +18,96 @@ const HomePage = () => {
     getLoggedInUserData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  const generateRandomData = () => {
+    const categories = ["Movies", "Shopping", "Dining", "Travel", "Utilities"];
+    const amounts = [-200, -400, -600, -800, -1000];
+    const dates = [
+      "May 3, 2024",
+      "June 10, 2024",
+      "July 15, 2024",
+      "August 20, 2024",
+      "September 25, 2024",
+    ];
+
+    return Array.from({ length: 10 }, () => ({
+      category: categories[Math.floor(Math.random() * categories.length)],
+      amount: amounts[Math.floor(Math.random() * amounts.length)],
+      date: dates[Math.floor(Math.random() * dates.length)],
+    }));
+  };
+  const data = generateRandomData();
   return (
-    <WithBackground>
-      <div className="flex justify-between text-white py-2 px-4">
-        <div>
-          <Greeting className="text-sm" />
-          <div className="text-xl font-semibold">{user.username}</div>
-        </div>
-        <div>
-          <Logout />
-        </div>
-      </div>
-      <div className="flex justify-center px-4">
-        <div className="w-[360px] h-[180px] bg-[#2f7e79] flex flex-col justify-between rounded-2xl shadow-lg shadow-[#2f7e79] p-4 text-white">
-          <div className="flex flex-col mb-5">
-            <span className="text-sm">Total balance</span>
-            <span className="text-3xl font-semibold">4,000</span>
+    <>
+      <div className="flex flex-col items-center justify-center h-dvh overflow-hidden">
+        <div className="flex justify-between text-white p-4 absolute top-2 left-0 w-full z-50">
+          <div>
+            <Greeting className="text-sm" />
+            <div className="text-xl font-semibold">{user.username}</div>
           </div>
-          <div className="flex justify-between">
-            <div className="flex flex-col">
-              <div className="flex gap-2 items-center">
-                <span className="text-sm text-slate-300">Income</span>
-                <Image src={IncomeArrow} alt="income show arrow" />
-              </div>
-              <span className="text-xl font-semibold">30000</span>
-            </div>
-            <div className="flex flex-col">
-              <div className="flex gap-2 items-center">
-                <span className="text-sm text-slate-300">Expense</span>
-                <Image src={ExpenseArrow} alt="income show arrow" />
-              </div>
-              <span className="text-xl font-semibold">30000</span>
-            </div>
+          <div>
+            <Logout />
           </div>
         </div>
+        <div className="absolute top-0 left-0 h-full">
+          <Image src={Bg} alt="background image" className="w-screen" />
+          <div className="relative bottom-[170px]  w-full flex flex-col h-full">
+            <div className="mb-4">
+              <HomeCard />
+            </div>
+            <div className="grow">
+              <span className="text-lg font-medium text-slate-500 px-4">
+                Transaction History
+              </span>
+              <ScrollArea className="w-full h-[90%] px-4">
+                {data.map((item, index) => (
+                  <div
+                    key={index + item.category.length}
+                    className="flex justify-between items-center border-b border-slate-100 my-1"
+                  >
+                    <div className="flex flex-col py-2">
+                      <div className="font-medium text-md">{item.category}</div>
+                      <div className="text-sm text-slate-400">{item.date}</div>
+                    </div>
+                    <div className="text-red-700 font-semibold">
+                      {item.amount}
+                    </div>
+                  </div>
+                ))}
+              </ScrollArea>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="my-3 flex flex-col">
-        <span className="text-lg font-medium text-slate-500 px-4">
-          Transaction History
-        </span>
-        <ScrollArea className="w-full h-[300px] px-4">
-          <div className="flex justify-between items-center border-b border-slate-100 my-1">
-            <div className="flex flex-col py-2">
-              <div className="font-medium text-md">Movies</div>
-              <div className="text-sm text-slate-400">May, 3 2024</div>
+    </>
+  );
+};
+
+const HomeCard = () => {
+  return (
+    <div className="flex w-full justify-center px-4">
+      <div className="w-full h-[180px] bg-[#2f7e79] flex flex-col justify-between rounded-2xl shadow-lg shadow-[#2f7e79] p-4 text-white">
+        <div className="flex flex-col mb-5">
+          <span className="text-sm">Total balance</span>
+          <span className="text-3xl font-semibold">4,000</span>
+        </div>
+        <div className="flex justify-between">
+          <div className="flex flex-col">
+            <div className="flex gap-2 items-center">
+              <span className="text-sm text-slate-300">Income</span>
+              <Image src={IncomeArrow} alt="income show arrow" />
             </div>
-            <div className="text-red-700 font-semibold"> - 400</div>
+            <span className="text-xl font-semibold">30000</span>
           </div>
-          <div className="flex justify-between items-center border-b border-slate-100 my-1">
-            <div className="flex flex-col py-2">
-              <div className="font-medium text-md">Movies</div>
-              <div className="text-xs text-slate-400">May, 3 2024</div>
+          <div className="flex flex-col">
+            <div className="flex gap-2 items-center">
+              <span className="text-sm text-slate-300">Expense</span>
+              <Image src={ExpenseArrow} alt="income show arrow" />
             </div>
-            <div className="text-red-700 font-semibold"> + 300</div>
+            <span className="text-xl font-semibold">30000</span>
           </div>
-          <div className="flex justify-between items-center border-b border-slate-100 my-1">
-            <div className="flex flex-col py-2">
-              <div className="font-medium text-md">Movies</div>
-              <div className="text-xs text-slate-400">May, 3 2024</div>
-            </div>
-            <div className="text-green-700 font-semibold"> - 400</div>
-          </div>
-          <div className="flex justify-between items-center border-b border-slate-100 my-1">
-            <div className="flex flex-col py-2">
-              <div className="font-medium text-md">Movies</div>
-              <div className="text-xs text-slate-400">May, 3 2024</div>
-            </div>
-            <div className="text-green-700 font-semibold"> + 500</div>
-          </div>
-          <div className="flex justify-between items-center border-b border-slate-100 my-1">
-            <div className="flex flex-col py-2">
-              <div className="font-medium text-md">Movies</div>
-              <div className="text-xs text-slate-400">May, 3 2024</div>
-            </div>
-            <div className="text-green-700 font-semibold"> + 600</div>
-          </div>
-        </ScrollArea>
+        </div>
       </div>
-    </WithBackground>
+    </div>
   );
 };
 

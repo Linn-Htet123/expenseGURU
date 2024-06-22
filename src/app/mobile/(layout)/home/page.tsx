448b04e7306ee2ee18f4312b3ed606123d5e7893
arkar93 <arkarkyaw.akk93@gmail.com>
2024-06-22 15:37:29 +0630

    feat: [GURU] wrap with suspense

--- src/app/mobile/(layout)/home/page.tsx
+++ src/app/mobile/(layout)/home/page.tsx
@@ -15,6 +15,7 @@ import { DotsHorizontalIcon } from "@radix-ui/react-icons";
 import TotalBalance from "@/components/common/totalBalance";
 import TransactionList from "@/components/mobile/transaction/transactionList";
 import { useEffect, useRef, useState } from "react";
+import WithSuspense from "@/components/common/withSuspense";
 
 const HomePage = () => {
   const { user } = useLogin();
@@ -35,50 +36,52 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center h-dvh overflow-hidden">
-        <div className="flex justify-between text-white p-4 absolute top-2 left-0 w-full z-50">
-          <div>
-            <Greeting className="text-sm" />
-            <div className="text-xl font-semibold">{user.username}</div>
-          </div>
-          <div>
+    <WithSuspense>
+      <div>
+        <div className="flex flex-col items-center justify-center h-dvh overflow-hidden">
+          <div className="flex justify-between text-white p-4 absolute top-2 left-0 w-full z-50">
             <div>
-              <Popover>
-                <PopoverTrigger>
-                  <div>
-                    <DotsHorizontalIcon fontSize={30} />
-                  </div>
-                </PopoverTrigger>
-                <PopoverContent className="w-24 mr-4 py-2 px-2">
-                  <Logout />
-                </PopoverContent>
-              </Popover>
+              <Greeting className="text-sm" />
+              <div className="text-xl font-semibold">{user.username}</div>
+            </div>
+            <div>
+              <div>
+                <Popover>
+                  <PopoverTrigger>
+                    <div>
+                      <DotsHorizontalIcon fontSize={30} />
+                    </div>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-24 mr-4 py-2 px-2">
+                    <Logout />
+                  </PopoverContent>
+                </Popover>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="absolute top-0 left-0 h-full overflow-hidden">
-          <Image
-            src={Bg}
-            alt="background image"
-            className="w-screen"
-            ref={imageRef}
-            priority
-          />
-          <div className="relative bottom-[170px]  w-full flex flex-col h-full">
-            <div className="mb-4">
-              <HomeCard />
-            </div>
-            <div className="grow">
-              <span className="text-lg font-medium text-slate-500 px-4">
-                Transaction History
-              </span>
-              <TransactionList height={listHeight} />
+          <div className="absolute top-0 left-0 h-full overflow-hidden">
+            <Image
+              src={Bg}
+              alt="background image"
+              className="w-screen"
+              ref={imageRef}
+              priority
+            />
+            <div className="relative bottom-[170px]  w-full flex flex-col h-full">
+              <div className="mb-4">
+                <HomeCard />
+              </div>
+              <div className="grow">
+                <span className="text-lg font-medium text-slate-500 px-4">
+                  Transaction History
+                </span>
+                <TransactionList height={listHeight} />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </WithSuspense>
   );
 };
 
